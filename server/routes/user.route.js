9d1db62042ed94588a8365a24db4422595f5f2d3
Sabid Rahman <136883738+sabid02@users.noreply.github.com>
2024-10-15 01:30:50 +0600

    api for get users

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUsers,
 } from "../controllers/user.controllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,5 +13,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
