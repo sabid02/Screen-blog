f55b445fa3d36801ab77dbc3743ceed3785f78f0
Sabid Rahman <136883738+sabid02@users.noreply.github.com>
2024-10-18 21:02:06 +0600

    dashboard section done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashUsers from "../components/DashUsers";
 import DashPosts from "../components/DashPosts";
 import DashComments from "../components/DashComments";
+import DashBoardComponents from "../components/DashBoardComponents";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -25,6 +26,7 @@ const Dashboard = () => {
       {tab == "posts" && <DashPosts />}
       {tab == "users" && <DashUsers />}
       {tab == "comments" && <DashComments />}
+      {tab == "dash" && <DashBoardComponents />}
     </div>
   );
 };
